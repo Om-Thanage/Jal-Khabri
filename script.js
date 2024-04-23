016767b6cfa8b5e5c7ebbacfa53073fbb8eed5e3
Om Thanage <om.thanage@somaiya.edu>
2024-04-23 14:01:12 +0530

    New database link

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
-import { getDatabase, onValue, ref } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-database.js";
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
+// import { getDatabase, onValue, ref } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-database.js";
 
-// Your web app's Firebase configuration
-const firebaseConfig = 
-    {   apiKey: "AIzaSyBIgjucBQT_UAWH4iD4JIBjCF79H9SeFrs",
-        authDomain: "water-monitoring-1856d.firebaseapp.com",
-        databaseURL: "https://water-monitoring-1856d-default-rtdb.asia-southeast1.firebasedatabase.app",
-        projectId: "water-monitoring-1856d",
-        storageBucket: "water-monitoring-1856d.appspot.com",
-        messagingSenderId: "1047258254470",
-        appId: "1:1047258254470:web:fcc420b6c7a3b74a885507"
-    };
+// // Your web app's Firebase configuration
+// const firebaseConfig = 
+//     {   apiKey: "AIzaSyBIgjucBQT_UAWH4iD4JIBjCF79H9SeFrs",
+//         authDomain: "water-monitoring-1856d.firebaseapp.com",
+//         databaseURL: "https://water-monitoring-1856d-default-rtdb.asia-southeast1.firebasedatabase.app",
+//         projectId: "water-monitoring-1856d",
+//         storageBucket: "water-monitoring-1856d.appspot.com",
+//         messagingSenderId: "1047258254470",
+//         appId: "1:1047258254470:web:fcc420b6c7a3b74a885507"
+//     };
+
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+  import { getDatabase, onValue, ref } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-database.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBUTb6lmztNxl_ZR2ryeu4Cx0uAu_HrpTE",
+    authDomain: "tds-sensor-ce7f0.firebaseapp.com",
+    databaseURL: "https://tds-sensor-ce7f0-default-rtdb.firebaseio.com",
+    projectId: "tds-sensor-ce7f0",
+    storageBucket: "tds-sensor-ce7f0.appspot.com",
+    messagingSenderId: "845308357837",
+    appId: "1:845308357837:web:6715bc4900adfaee010d84",
+    measurementId: "G-PMZV98SVXT"
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
